425b615c47fab4ea2cf90fbd7fe4590562c94a63
Mou <mohamed.m@trufla.com>
2018-09-05 11:43:31 +0200

    adding different use cases

--- src/app/offline/offline.component.ts
+++ src/app/offline/offline.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { environment } from '../../environments/environment.prod';
 
 @Component({
   selector: 'app-offline',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfflineComponent implements OnInit {
 
-  constructor() { }
+  searchTerm = '';
+  public data = this.route.snapshot.data.data;
+  constructor(private apiService: ApiService, private route: ActivatedRoute, private router: Router) {
+  }
 
   ngOnInit() {
   }
 
+  onChange() {
+    if (!this.searchTerm) {
+      this.apiService.getData(environment.apiUrl, 'Progressive Web Apps').subscribe((resp) => {
+        this.data = resp;
+      });
+    } else {
+      this.apiService.getData(environment.apiUrl, this.searchTerm).subscribe((resp) => {
+        this.data = resp;
+      });
+    }
+
+  }
+
+  readBook(url) {
+    window.open(url, '_blank');
+  }
 }
